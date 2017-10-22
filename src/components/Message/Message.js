e2127a42821d20a3b1b24b60a492c9bcb0c586d8
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2017-10-22 21:11:56 +0200

    remove message functionality: done

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import {createFragmentContainer, graphql} from 'react-relay';
+import IconButton from 'material-ui/IconButton';
+import DeleteIcon from 'material-ui-icons/Delete';
+import {withStyles} from 'material-ui/styles';
+import RemoveMessageMutation from '../../mutations/RemoveMessageMutation';
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit
+    }
+});
 
 class Message extends React.Component {
     state = {
         isEditing: false
     };
+    removeMessage = () => {
+        RemoveMessageMutation.commit(this.props.relay.environment, this.props.message, this.props.viewer);
+    };
 
     render() {
-        return <div>{this.props.message.text}</div>;
+        const {classes} = this.props;
+        return (
+            <div>
+                {this.props.message.text}
+                <IconButton className={classes.button} aria-label="Delete" onClick={this.removeMessage}>
+                    <DeleteIcon />
+                </IconButton>
+            </div>
+        );
     }
 }
 
-export default createFragmentContainer(Message, {
+export default createFragmentContainer(withStyles(styles)(Message), {
     message: graphql`
         fragment Message_message on Message {
             id
